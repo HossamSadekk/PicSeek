0df729b3ec6bf8d9682851af9f0295bb48953baa
HossamSadekk <66652255+HossamSadekk@users.noreply.github.com>
2022-07-15 12:55:11 -0700

    adding module with providing retrofit instance & injecting repository and viewModels

--- app/src/main/java/com/example/imagesapp/ui/fragments/GalleryFragment.kt
+++ app/src/main/java/com/example/imagesapp/ui/fragments/GalleryFragment.kt
@@ -2,7 +2,9 @@ package com.example.imagesapp.ui.fragments
 
 import androidx.fragment.app.Fragment
 import com.example.imagesapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
 }
